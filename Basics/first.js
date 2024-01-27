86aa8468fa25a44e8876074a38290c70b56f9221
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-01-27 08:56:52 +0530

    create module for datetime

--- Basics/first.js
+++ Basics/first.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var dt = require('./datatime');
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write("The date and time are currently: " + dt.myDateTime());
     res.end('Hello World! Welcome to Node.js world');
 }).listen(8080);
