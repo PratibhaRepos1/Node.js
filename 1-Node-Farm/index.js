ed1ba42f3bc6a558372bcfc277f8a28c8a55f4bf
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-02-23 20:04:12 +0700

    Non-blocking, asynchronous way

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -1,17 +1,32 @@
-const hello = 'Hello world';
-console.log(hello);
+// const hello = 'Hello world';
+// console.log(hello);
 
-const fs = require('fs');
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
+//Blocking, Synchronous way
+ const fs = require('fs');
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(textIn);
+// //write to file
+// const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now.toString()} `;
+// fs.writeFileSync('./txt/output.txt', textIn);
+// console.log('File Written');
 
-//write to file
-const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now.toString()} `;
+//Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('ERROR! 🌋');
+    
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+        console.log(data3);
 
-fs.writeFileSync('./txt/output.txt', textIn);
+        fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+            console.log('Your file has been written 😊');
+        })
+        });
+    });    
+});
 
-console.log('File Written');
-
+console.log('will read file');
 
 
 
